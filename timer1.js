881519c8b43a20a2ec67b9cf1db670f4e5184a24
vagrant <rpg7ofdeath@gmail.com>
2021-08-25 02:26:24 +0000

    added filtering

--- timer1.js
+++ timer1.js
@@ -1,8 +1,6 @@
 //sys sound does not work
 
-let cliArgs = process.argv.slice(2).sort(function (a, b) {
-  return a - b
-})
+let cliArgs = process.argv.slice(2).sort((a, b) => a - b).filter(x => x > -1)
 
 return cliArgs.map(element => 
   setTimeout(function() {
